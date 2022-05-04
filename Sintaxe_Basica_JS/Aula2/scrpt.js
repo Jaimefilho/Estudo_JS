5e816d4ef66b6e924a29ce313fd1aa2f94b7b86b
jaime <jaime.osmar.rodrigues.filho@gmail.com>
2022-05-04 10:50:39 -0400

    Aula de Sinataxe Basica do JS feita

---
+++ Sintaxe_Basica_JS/Aula2/scrpt.js
@@ -0,0 +1,40 @@
+// Tipos Primitivos 
+
+//boolean
+var vOuF = false;
+console.log(typeof(vOuF))
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer))
+
+//string
+var numeroQualquer = 'Jaime'
+console.log(typeof(nome))
+
+//function
+var funcao = function() {}
+console.log(typeof(funcao))
+
+//Variavel (var) varia conforme o código , Constnate (const) não varia conforme o código e o Let só exite dentro de um bloco de texto  
+
+// atribuição
+var atribuicao = 'Jaime';
+
+//comparação
+var comparacao = '0' == 0 ;
+console.log(comparacao)
+
+//comparacao identica
+var comparacaoIdentica = '0' === 0 ;
+console.log(comparacaoIdentica)
+
+//adicao
+var adicao = 1+1;
+console.log(adicao)
+
+//subtracao
+var subtracao = 2 -1;
+console.log(subtracao)
+
+//multiplicação *, Divisão / , resto % ,  Maior que > menor que < , maior igual >=, Menor igual <= , e && , || ou, ! não
